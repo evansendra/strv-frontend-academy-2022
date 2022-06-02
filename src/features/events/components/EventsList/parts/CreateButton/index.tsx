7438fd261f1ad8e52302aa211ff124a4ca9562e5
Evan <evan@evansendra.com>
2022-06-02 09:00:19 +0200

    fix: add tabIndex to create event button

--- src/features/events/components/EventsList/parts/CreateButton/index.tsx
+++ src/features/events/components/EventsList/parts/CreateButton/index.tsx
@@ -5,6 +5,8 @@ import { Routes } from '~/features/core/constants/routes'
 
 export const CreateButton: FC = () => (
   <Link href={Routes.CREATE_EVENT}>
-    <a aria-label="Create Event">+</a>
+    <a aria-label="Create Event" tabIndex={0}>
+      +
+    </a>
   </Link>
 )
