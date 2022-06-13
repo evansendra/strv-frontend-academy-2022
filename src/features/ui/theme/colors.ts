3640f1f1c31c75242ae21eca573252036f55fbf3
Evan <evan@evansendra.com>
2022-06-13 15:52:35 +0200

    feat: add colors to theme

--- src/features/ui/theme/colors.ts
+++ src/features/ui/theme/colors.ts
@@ -1,7 +1,22 @@
 import { palette } from './palette'
 
 export const colors = {
-  // backgrounds
-  bgPrimary: palette.lightGrey,
-  bgSecondary: palette.white,
+  text: {
+    base: palette.gray[100],
+    dim: palette.gray[500],
+    light: palette.gray[800],
+    inverse: palette.white,
+  },
+  background: {
+    light: palette.white,
+    slight: palette.gray[100],
+    dim: palette.gray[600],
+    dark: palette.gray[900],
+  },
+  accent: {
+    primary: palette.green.light,
+    primaryHover: palette.green.dark,
+    danger: palette.red.light,
+    dangerHover: palette.red.dark,
+  },
 }
--- src/features/ui/theme/palette.ts
+++ src/features/ui/theme/palette.ts
@@ -1,9 +1,21 @@
 export const palette = {
-  // shades of gray
-  lightGrey: '#F9F9FB',
-
   // generic colors
   white: '#FFF',
   black: '#000',
-  transparent: 'transparent',
+
+  gray: {
+    100: '#323C46',
+    500: '#949EA8',
+    600: '#C4C9D1',
+    800: '#D9DCE1',
+    900: '#F9F9FB',
+  },
+  red: {
+    light: '#FF4081',
+    dark: '#E73370',
+  },
+  green: {
+    light: '#22D486',
+    dark: '#20BD78',
+  },
 }
